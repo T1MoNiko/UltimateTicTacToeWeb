260ae93d0440fdf77e7b1b29da8ef3c6dcbb98e8
Vlad <lenovot1990@gmail.com>
2024-01-09 16:54:53 +0300

    Компонент крестика

---
+++ my-app/src/components/Cross.jsx
@@ -0,0 +1,13 @@
+import React from "react"
+import "../styles/cross.scss"
+
+const Cross = () => {
+    return (
+    <div className="container">
+        <div className="line"></div>
+        <div className="line"></div>
+    </div>
+    )
+}
+
+export default Cross;
