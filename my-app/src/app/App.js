d3d7e141e010416e6c76921becb5cff14d57807d
Vlad <lenovot1990@gmail.com>
2024-01-10 00:32:13 +0300

    главные стили и измененеие главного файла

--- my-app/src/app/App.js
+++ my-app/src/app/App.js
@@ -1,12 +1,18 @@
 import React from "react";
 import Cross from "../components/Cross";
+import Circle from "../components/Circle";
+import Cell from "../components/Cell";
+import Box from "../components/Box";
+import Playground from "../components/PLayground";
 
 const App = () => {
     return(
       <>
-        <Cross/>
+        <Playground/>
       </>
     )
 }
 
 export default App;
+
+
