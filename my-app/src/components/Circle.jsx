59312053467485484b0c58c11475bfba0e32fc2a
Vlad <lenovot1990@gmail.com>
2024-01-10 00:08:58 +0300

    добавление кружочка

---
+++ my-app/src/components/Circle.jsx
@@ -0,0 +1,12 @@
+import React from "react"
+import "../styles/circle.scss"
+
+const Circle = () => {
+    return (
+    <>
+        <div className="circle"></div>
+    </>
+    )
+}
+
+export default Circle;
