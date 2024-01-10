0a70a7b7d385173676d89f895298fdd5fa03cfd8
Vlad <lenovot1990@gmail.com>
2024-01-11 00:32:06 +0300

    Обмен данными логика по смнене крестиков и ноликов в ячейке отправка о инидексах крестиков и ноликов в ячейке в файл бокса

--- my-app/src/components/Cell.jsx
+++ my-app/src/components/Cell.jsx
@@ -4,18 +4,18 @@ import Circle from "./Circle";
 
 const Cell = React.memo((props) => {
     const [state, setState] = useState(3);
-    const {player, funcSet} = props;
+    const {player, funcSet, index, funcSetCross, funcSetCircle, prevCircle, prevCross} = props;
 
     const changeSymbol = (player, setPlayer) => {
         if (Number(state) !== 1 && Number(state) !== 0) {
             setState(player)
+            player ? funcSetCross([...prevCross, index]) : funcSetCircle([...prevCircle, index])
             setPlayer(!player)
         }
     }
 
     return (
         <div className="cell" onClick={() => changeSymbol(player, funcSet)}>
-
             {Number(state) === 1 ? <Cross/> : !state ? <Circle/> : null}
         </div>
     );
