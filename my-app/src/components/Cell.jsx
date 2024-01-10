1a2f279e278b54053bb6659ef1d9109488ea6445
Vlad <lenovot1990@gmail.com>
2024-01-10 11:43:25 +0300

    Логика для хода игрока и исправление ошибки при клике на одну и ту же ячейку

--- my-app/src/components/Cell.jsx
+++ my-app/src/components/Cell.jsx
@@ -2,13 +2,21 @@ import React, { useState } from "react";
 import Cross from "./Cross";
 import Circle from "./Circle";
 
-const Cell = React.memo(() => {
-    const [state, setState] = useState(0);
+const Cell = React.memo((props) => {
+    const [state, setState] = useState(3);
+    const {player, funcSet} = props;
+
+    const changeSymbol = (player, setPlayer) => {
+        if (Number(state) !== 1 && Number(state) !== 0) {
+            setState(player)
+            setPlayer(!player)
+        }
+    }
 
     return (
-        <div className="cell" onClick={() => setState(2)}>
-            {state === 1 ? <Cross/> : null}
-            {state === 2 ? <Circle/> : null}
+        <div className="cell" onClick={() => changeSymbol(player, funcSet)}>
+
+            {Number(state) === 1 ? <Cross/> : !state ? <Circle/> : null}
         </div>
     );
 }
