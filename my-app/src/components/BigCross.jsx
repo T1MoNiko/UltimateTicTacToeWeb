0dad1b4108cb8b8e4977ebac85e5678d02dc4e88
Vlad <lenovot1990@gmail.com>
2024-01-11 00:23:15 +0300

    Добавление компонента большого кретсика

---
+++ my-app/src/components/BigCross.jsx
@@ -0,0 +1,12 @@
+import "../styles/cross.scss"
+
+const BigCross = () => {
+    return (
+    <div className="container-big-cross">
+        <div className="line"></div>
+        <div className="line"></div>
+    </div>
+    )
+}
+
+export default BigCross;
