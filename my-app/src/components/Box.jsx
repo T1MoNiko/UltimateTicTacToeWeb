0339bccb837d7c4a0cff18634955f9554c06f85b
stoopotec <stoopotec@tuta.io>
2024-01-11 17:46:34 +0300

    Сделал читаемый кодnpm start

--- my-app/src/components/Box.jsx
+++ my-app/src/components/Box.jsx
@@ -10,13 +10,13 @@ const Box = React.memo((props) => {
     const flag = useRef({
                         win: false,
                         whoWin: null
-                     }),
-          countOfCross = useRef(0),
-          countOfCircle = useRef(0),
-          [indexOfCross, setIndexOfCross] = useState([]),
-          [indexOfCircle, setIndexOfCircle] = useState([]),
-          update = useRef(true),
-          winningCombination = [[0, 4, 8], [2, 4, 6],
+                     });
+    const countOfCross = useRef(0);
+    const countOfCircle = useRef(0);
+    const [indexOfCross, setIndexOfCross] = useState([]);
+    const [indexOfCircle, setIndexOfCircle] = useState([]);
+    const update = useRef(true);
+    const winningCombination = [[0, 4, 8], [2, 4, 6],
                                 [0, 1, 2], [3, 4, 5],
                                 [6, 7, 8], [0, 3, 6],
                                 [1, 4, 7], [2, 5, 8]]; 
@@ -49,24 +49,24 @@ const Box = React.memo((props) => {
         if (flag.current.win) {
             flag.current.whoWin === 'cross' ? funcSetBigCross([...prevBigCross, index]) : funcSetBigCircle([...prevBigCircle, index]);
         }
-    //eslint-disable-next-line
-}, [indexOfCross, indexOfCircle])
+        //eslint-disable-next-line
+    }, [indexOfCross, indexOfCircle]);
 
-return (
-    flag.current.win ? flag.current.whoWin === 'cross' ? <BigCross/> : <BigCircle/> :
-        <div className="box-wrapper">
-            {[...new Array(9)].map((_, id) => {
-                return <Cell key={id} 
-                             funcSet={funcSet} 
-                             player={player} 
-                             index={id}
-                             funcSetCross={setIndexOfCross} 
-                             funcSetCircle={setIndexOfCircle}
-                             prevCircle={indexOfCircle}
-                             prevCross={indexOfCross}
-                             />
-            })}
-        </div>
+    return (
+        flag.current.win ? flag.current.whoWin === 'cross' ? <BigCross/> : <BigCircle/> :
+            <div className="box-wrapper">
+                {[...new Array(9)].map((_, id) => {
+                    return <Cell key={id} 
+                                funcSet={funcSet} 
+                                player={player} 
+                                index={id}
+                                funcSetCross={setIndexOfCross} 
+                                funcSetCircle={setIndexOfCircle}
+                                prevCircle={indexOfCircle}
+                                prevCross={indexOfCross}
+                                />
+                })}
+            </div>
         )
     }
 )
